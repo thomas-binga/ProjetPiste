25799ca2c9f8600e3a3ea5bea015097c6d0905a5
Tozz69 <tom.lostuzzo@etu.univ-lyon1.fr>
2022-06-15 21:39:41 +0200

    Updated Accueil.js, FooterComponent.js and HeaderComponent.js

---
+++ front/src/components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FooterComponent = () => {
+    return (
+        <div>
+            <footer className="footer bg-dark">
+                <span className="text-muted">Gaëtan SCART, Tom LOSTUZZO et Thomas BINGA</span>
+            </footer>
+        </div>
+    );
+};
+
+export default FooterComponent;
